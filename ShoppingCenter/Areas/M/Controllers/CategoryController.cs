97cd95ea9424035d728099fd72a2289e8db4311a
FringeXE <fringexe@163.com>
2015-04-29 18:06:54 +0800

    Add Category

---
+++ ShoppingCenter/Areas/M/Controllers/CategoryController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCenter.Areas.M.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: M/Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: M/Category/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: M/Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: M/Category/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: M/Category/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: M/Category/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: M/Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: M/Category/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ShoppingCenter/Areas/P/PAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ShoppingCenter.Areas.P
+{
+    public class PAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "P";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "P_default",
+                "P/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
